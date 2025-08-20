38d6d342d218623bb3e351e1689039b2375b064c
camilofinal <camilofinal@gmail.com>
2025-08-20 13:56:35 +0000

    add productData database

---
+++ src/data/productData.js
@@ -0,0 +1,143 @@
+// src/data/productData.js
+
+
+const products = [
+    // Fruits & Vegetables
+    {
+      id: 1,
+      name: "Bananas",
+      category: "Fruits & Vegetables",
+      price: 1.99,
+      image: "/assets/images/Fruits_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 2,
+      name: "Apples",
+      category: "Fruits & Vegetables",
+      price: 2.49,
+      image: "/assets/images/Fruits_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 3,
+      name: "Tomatoes",
+      category: "Fruits & Vegetables",
+      price: 1.79,
+      image: "/assets/images/Fruits_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 4,
+      name: "Carrots",
+      category: "Fruits & Vegetables",
+      price: 1.29,
+      image: "/assets/images/Fruits_category.PNG",
+      isOffer: false,
+    },
+  
+    // Dairy & Eggs
+    {
+      id: 5,
+      name: "Milk",
+      category: "Dairy & Eggs",
+      price: 0.99,
+      image: "/assets/images/dairy_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 6,
+      name: "Cheese",
+      category: "Dairy & Eggs",
+      price: 3.49,
+      image: "/assets/images/dairy_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 7,
+      name: "Yogurt",
+      category: "Dairy & Eggs",
+      price: 1.19,
+      image: "/assets/images/dairy_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 8,
+      name: "Eggs (12 pack)",
+      category: "Dairy & Eggs",
+      price: 2.29,
+      image: "/assets/images/dairy_category.PNG",
+      isOffer: false,
+    },
+  
+    // Household Cleaning
+    {
+      id: 9,
+      name: "Dish Soap",
+      category: "Household Cleaning",
+      price: 1.99,
+      image: "/assets/images/cleaning_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 10,
+      name: "Laundry Detergent",
+      category: "Household Cleaning",
+      price: 4.99,
+      image: "/assets/images/cleaning_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 11,
+      name: "All-Purpose Cleaner",
+      category: "Household Cleaning",
+      price: 3.49,
+      image: "/assets/images/cleaning_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 12,
+      name: "Paper Towels",
+      category: "Household Cleaning",
+      price: 2.99,
+      image: "/assets/images/cleaning_category.PNG",
+      isOffer: false,
+    },
+  
+    // Drinks & Snacks
+    {
+      id: 13,
+      name: "Cola Soda",
+      category: "Drinks & Snacks",
+      price: 1.49,
+      image: "/assets/images/drinks_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 14,
+      name: "Orange Juice",
+      category: "Drinks & Snacks",
+      price: 2.19,
+      image: "/assets/images/drinks_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 15,
+      name: "Potato Chips",
+      category: "Drinks & Snacks",
+      price: 1.99,
+      image: "/assets/images/drinks_category.PNG",
+      isOffer: false,
+    },
+    {
+      id: 16,
+      name: "Chocolate Bar",
+      category: "Drinks & Snacks",
+      price: 1.29,
+      image: "/assets/images/drinks_category.PNG",
+      isOffer: false,
+    },
+  ];
+  
+  export default products;
+  
